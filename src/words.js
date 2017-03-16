c1264125867efe9d5920e4c25a30d8dc9af17f4a
Maranatha A. Ilesanmi <mbilesanmi@users.noreply.github.com>
2017-03-16 10:51:52 +0100

    Update words.js

--- src/words.js
+++ src/words.js
@@ -3,14 +3,14 @@
  */
 
 
-function words(n){
-    var splitString = n.split(/\s+|\n/m);
+function words(string){
+    var splitString = string.split(/\s+|\n/m);
     var arr = {};
-    function stringCount(n, word){
+    function stringCount(string, word){
         var count = 0;
-        string = n.split(/\s+|\n/m);
-        for (var i = 0; i < string.length; i++) {
-            if (string[i] === word){
+        str = string.split(/\s+|\n/m);
+        for (var i = 0; i < str.length; i++) {
+            if (str[i] === word){
                 count++;
             }
         }
@@ -19,7 +19,7 @@ function words(n){
     for (var i = 0; i < splitString.length; i++) {
         if (!arr.hasOwnProperty(splitString[i])){
             var value = splitString[i];
-            arr[splitString[i]] = stringCount(n, splitString[i]);
+            arr[splitString[i]] = stringCount(string, splitString[i]);
 
         }
     }
